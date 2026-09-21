e727d411a0d00baa188c2c2240f676dbfd09ff72
rengganurardiyansah2004 <rengganurardiyansah@student.uir.ac.id>
2026-09-21 20:58:25 +0700

    Configure project for Vercel deployment with Cloud DB support

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,12 +1,13 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "db_repository_dokumen";
+$host     = getenv('DB_HOST') ?: (getenv('MYSQLHOST') ?: "localhost");
+$user     = getenv('DB_USER') ?: (getenv('MYSQLUSER') ?: "root");
+$password = getenv('DB_PASSWORD') ?: (getenv('MYSQLPASSWORD') ?: "");
+$database = getenv('DB_NAME') ?: (getenv('MYSQLDATABASE') ?: "db_repository_dokumen");
+$port     = getenv('DB_PORT') ?: (getenv('MYSQLPORT') ?: 3306);
 
-$koneksi = mysqli_connect($host, $user, $password, $database);
+$koneksi = mysqli_connect($host, $user, $password, $database, (int)$port);
 
 if (!$koneksi) {
     die("Koneksi gagal : " . mysqli_connect_error());
-}
+}
